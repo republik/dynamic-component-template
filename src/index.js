fb97671a5f5068c95ceaa1625b0e712bc1096a6d
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-09-22 20:27:59 +0200

    add gql example

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { css } from 'glamor'
 import { mediaQueries } from '@project-r/styleguide'
 import ChartExample from './ChartExample'
+import Query from './Query'
 
 // const ASSETS_BASE_URL = 'https://cdn.repub.ch/s3/republik-assets/dynamic-components/REPOSLUG/assets'
 
 const Index = ({ type, text }) => {
   if (type === 'ChartExample') {
     return <ChartExample />
+  }
+  if (type === 'Query') {
+    return <Query />
   }
   return (
     <div
